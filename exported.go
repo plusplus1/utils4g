3f75b14f3395ce9e8d94e88b585923b42bc2d874
plusplus1 <comeson@126.com>
2019-04-17 17:05:11 +0800

    del util

--- exported.go
+++ exported.go
@@ -1,9 +1,30 @@
 package utils4g
 
-var (
-	DebugUtils    = newDebugUtil()
-	ConfigUtils   = newConfigUtil()
-	StrUtils      = newStrUtil()
-	DateTimeUtils = newDatetimeUtil()
-	DatabaseUtils = newDbUtil()
+import (
+	"github.com/plusplus1/utils4g/configuration"
+	"github.com/plusplus1/utils4g/env"
+	"github.com/plusplus1/utils4g/stdlog"
 )
+
+var StdLog = stdlog.Std
+
+func ISDebug() bool {
+	return env.ISDebug()
+}
+
+func ISDocker() bool {
+	return env.ISDocker()
+}
+func BaseDir() string {
+	return env.BaseDir()
+}
+func ConfDir() string {
+	return env.ConfDir()
+}
+func LoadAbsYamlConf(yaml string, out interface{}) error {
+	return configuration.LoadYaml(yaml, out)
+}
+
+func LoadRelativePathConf(path string, out interface{}) error {
+	return configuration.LoadRelativePath(path, out)
+}
--- types.go
+++
@@ -1,8 +0,0 @@
-package utils4g
-
-type (
-	dbUtil struct {
-		Mongo *dbMgoUtil
-		Mysql *dbMysqlUtil
-	}
-)
